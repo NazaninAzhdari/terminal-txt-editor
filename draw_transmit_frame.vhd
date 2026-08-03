5dbb7132f718a06cb6e31f5977e64febc8b17c78
Naz <Nazanin.Azhdari.sbu@gmail.com>
2026-08-03 16:12:20 +0300

    drawing the tansmission frame

---
+++ draw_transmit_frame.vhd
@@ -0,0 +1,84 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library work;
+use work.font_pack.ALL;
+
+entity draw_transmit_frame is
+    port (
+        i_clk               :   in      STD_LOGIC;
+        i_reset             :   in      STD_LOGIC;
+        i_x                 :   in      unsigned(9 downto 0);
+        i_y                 :   in      unsigned(9 downto 0);
+        o_draw_TX_frame     :   out     STD_LOGIC
+    );
+end draw_transmit_frame;
+
+architecture RTL of draw_transmit_frame is
+    signal r_x_div_4    :   integer range 0 to 159  :=0;
+    signal r_y_div_4    :   integer range 0 to 119  :=0;
+    signal r_x_div_32   :   integer range 0 to 19   :=0;
+    signal r_y_div_32   :   integer range 0 to 14   :=0;
+
+    begin
+        r_x_div_4 <= to_integer(i_x(9 downto 2));
+        r_y_div_4 <= to_integer(i_y(9 downto 2));
+        r_x_div_32 <= to_integer(i_x(9 downto 5));
+        r_y_div_32 <= to_integer(i_y(9 downto 5));
+
+        process(i_clk, i_reset) is
+			begin
+            if i_reset = '1' then
+                o_draw_TX_frame <= '0';
+
+            elsif rising_edge(i_clk) then
+                -----------------------------------------------------------------------------
+                -- Display "Transmitting . . ." in the Row of 6 (out of 15 Row => 480/32 = 15)
+                -----------------------------------------------------------------------------
+                if r_y_div_32 = 6 then
+                    case r_x_div_32 is
+                        when  2 =>
+                            o_draw_TX_frame <= pc_DRAW_CAPITAL_T(r_y_div_4 - 48)(r_x_div_4 - 16); --48 = 6*32/4 and 16 = 2*32/4
+                        when  3 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_r(r_y_div_4 - 48)(r_x_div_4 - 24);
+                        when  4 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_a(r_y_div_4 - 48)(r_x_div_4- 32);
+                        when  5 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_n(r_y_div_4 - 48)(r_x_div_4- 40);
+                        when  6 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_s(r_y_div_4 - 48)(r_x_div_4- 48);
+                        when  7 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_m(r_y_div_4 - 48)(r_x_div_4- 56);
+                        when  8 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_i(r_y_div_4 - 48)(r_x_div_4- 64);
+                        when  9 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_t(r_y_div_4 - 48)(r_x_div_4- 72);
+                        when  10 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_t(r_y_div_4 - 48)(r_x_div_4- 80);
+                        when  11 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_i(r_y_div_4 - 48)(r_x_div_4- 88);
+                        when  12 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_n(r_y_div_4 - 48)(r_x_div_4- 96);
+                        when  13 =>
+                            o_draw_TX_frame <= pc_DRAW_SMALL_g(r_y_div_4 - 48)(r_x_div_4- 104);
+                        when  14 =>
+                            o_draw_TX_frame <= pc_DRAW_PERIOD(r_y_div_4 - 48)(r_x_div_4- 112);
+                        when  15 =>
+                            o_draw_TX_frame <= pc_DRAW_PERIOD(r_y_div_4 - 48)(r_x_div_4- 120);
+                        when  16 =>
+                            o_draw_TX_frame <= pc_DRAW_PERIOD(r_y_div_4 - 48)(r_x_div_4- 128);
+                        when others => 
+                            o_draw_TX_frame <= '0';
+                    end case;
+
+                else
+                    o_draw_TX_frame <= '0';
+                end if;
+            end if;
+        end process;
+
+
+    end RTL;
+
+    
